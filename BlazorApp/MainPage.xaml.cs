735a19cc67b3ec90daae3f8cf4e4285380a278ce
Sangeeth Nandakumar <twileloop@outlook.com>
2023-03-14 00:52:26 +0530

    feat - Removed Android overscroll

--- BlazorApp/MainPage.xaml.cs
+++ BlazorApp/MainPage.xaml.cs
@@ -1,9 +1,19 @@
-﻿namespace BlazorApp;
+﻿using Microsoft.AspNetCore.Components.WebView;
+
+namespace BlazorApp;
 
 public partial class MainPage : ContentPage
 {
 	public MainPage()
 	{
 		InitializeComponent();
-	}
+        blazorWebView.BlazorWebViewInitialized += BlazorWebView_BlazorWebViewInitialized;
+    }
+
+    private void BlazorWebView_BlazorWebViewInitialized(object sender, BlazorWebViewInitializedEventArgs e) {
+        #if ANDROID
+            // Disable overscroll on Android
+            e.WebView.OverScrollMode = Android.Views.OverScrollMode.Never;
+        #endif
+    }
 }
